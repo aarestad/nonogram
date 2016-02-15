482a32d585c51a9a363d713991b55c0ada1cfddd
Peter Aarestad <aarestad@asgard.attlocal.net>
2016-02-14 20:20:20 -0600

    angular profile

---
+++ grails-app/assets/javascripts/nonogram/nonogram.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /nonogram/core/nonogram.core
+//= require /nonogram/index/nonogram.index
+
+angular.module("nonogram", [
+    "nonogram.core",
+    "nonogram.index"
+]);
